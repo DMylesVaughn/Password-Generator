e89d5d49afd0656856209386b8ad006d926afabe
Danny Myles Vaughn <142174754+DMylesVaughn@users.noreply.github.com>
2023-09-10 20:06:45 -0400

    Add files via upload
    Javascript completed after fixing the bug related to printed the final password generated.
    README still needs added live URL.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Password-Generator
-Password Generator_Module3
+
+The purpose of this project was to develop a random password generator using javascript.
+
+## User Story
+    AS AN employee with access to sensitive data
+    I WANT to randomly generate a password that meets certain criteria
+    SO THAT I can create a strong password that provides greater security
+
+## Acceptance Criteria
+    GIVEN I need a new, secure password
+    WHEN I click the button to generate a password
+    THEN I am presented with a series of prompts for password criteria
+    WHEN prompted for password criteria
+    THEN I select which criteria to include in the password
+    WHEN prompted for the length of the password
+    THEN I choose a length of at least 8 characters and no more than 128 characters
+    WHEN asked for character types to include in the password
+    THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+    WHEN I answer each prompt
+    THEN my input should be validated and at least one character type should be selected
+    WHEN all prompts are answered
+    THEN a password is generated that matches the selected criteria
+    WHEN the password is generated
+    THEN the password is either displayed in an alert or written to the page
+
+The following image is the initial load page for the Password Generator.
+![password-generator load page](./assets/Images/Load_Page.png)
+
+The following image is the password length prompt for the Password Generator.
+![password-generator length](./assets/Images/Pswd_lgth.png)
+
+The following image if the error message for password length if value is out of range.
+![password-generator length error](./assets/Images/Pswd_lgth_error.png)
+
+The following image is the character selector prompt.
+![password-generator char select](./assets/Images/char_selectors.png)
+
+The following image is the error message if no character selectors are chosen.
+![password-generator char select error](./assets/Images/char_selector_error.png)
+
+The following image is the end result from the password generator.
+![password-generator result](./assets/Images/result_pswd_gen.png)
+
+## Deployed Link
+
--- script.js
+++ script.js
@@ -1,14 +1,66 @@
 // Assignment Code
+// Query selectors for generate and password
 var generateBtn = document.querySelector("#generate");
+var password = document.querySelector("#password");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+// variables for all character choices
+var special = "!@#$%^&*()";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var optionsVariable = "";
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Write a function that generates a random password in the "text area" box
+function writePassword() {
+   password.value === "";
+   var randomPassword = "";
+
+    // Set prompt and criteria for password length
+    var passwordLength = prompt("Select a password length between 8 and 128 characters.");
+      if (passwordLength < 8 || passwordLength > 128) {
+        alert("Error--Password length must be between 8 and 128 characters!");
+        var passwordLength = prompt("Select a password length between 8 and 128 characters.");
+  }
+
+    // Set choices and restrictions for character selectors
+    var lowercaseLett = confirm("Should the password contain lowercase letters?");
+    var uppercaseLett = confirm("Should the password contain uppercase letters?");
+    var num = confirm("Should the password contain numbers?");
+    var specialChar = confirm("Should the password contain special characters?");
+      if (lowercaseLett === false && uppercaseLett === false && num === false && specialChar === false) {
+        alert("Error--Password must contain at least one character selector criteria!");
+        var lowercaseLett = confirm("Should the password contain lowercase letters?");
+        var uppercaseLett = confirm("Should the password contain uppercase letters?");
+        var num = confirm("Should the password contain numbers?");
+        var specialChar = confirm("Should the password contain special characters?");
+  }
+
+      // Use the character selectors selected to generate password
+    if (lowercaseLett) {
+        optionsVariable += lowercase;
+    }
+
+    if (uppercaseLett) {
+        optionsVariable += uppercase;
+    }
+
+    if (num) {
+        optionsVariable += numbers;
+    }
+
+    if (specialChar) {
+        optionsVariable += special;
+    }
+
+    // Generates a random password from user criteria for character selectors and length
+    for (var i = 0; i < passwordLength; i++) {
+        
+      // Ensures length of password
+        randomPassword += optionsVariable.charAt(Math.floor(Math.random() * optionsVariable.length));
+    }
+    password.value = randomPassword;
+
+}
